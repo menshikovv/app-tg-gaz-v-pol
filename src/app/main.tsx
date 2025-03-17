1f7f5ae994251bb3363064ddca7f45db6e6e740d
menshikovv <matveydrobot15@gmail.com>
2025-03-17 22:44:45 +0300

    lslslsld

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -1,10 +1,10 @@
 import { createRoot } from 'react-dom/client'
 import { App } from './routes/AppRouter.tsx'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import './index.scss'
 
 createRoot(document.getElementById('root')!).render(
-    <BrowserRouter>
+    <HashRouter>
         <App />
-    </BrowserRouter>
+    </HashRouter>
 )
