3a43b07fbfe4ba679af97144a3abcc2bd70f7182
menshikovv <matveydrobot15@gmail.com>
2025-03-17 23:20:39 +0300

    sadffax

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -1,10 +1,10 @@
 import { createRoot } from 'react-dom/client'
 import { App } from './routes/AppRouter.tsx'
-import { MemoryRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import './index.scss'
 
 createRoot(document.getElementById('root')!).render(
-    <MemoryRouter>
+    <HashRouter>
         <App />
-    </MemoryRouter>
+    </HashRouter>
 )
