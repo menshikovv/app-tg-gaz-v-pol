bc9299a8abf562de9efb9e269de58de1705628e3
menshikovv <matveydrobot15@gmail.com>
2025-03-19 01:05:27 +0300

    lsc

--- src/page/Promo/Promo.tsx
+++ src/page/Promo/Promo.tsx
@@ -21,32 +21,38 @@ export const Promo = () => {
             duration: 1,
         });
 
-
         setTimeout(() => {
             setIsLoading(false);
         }, 1000);
     };
 
     useEffect(() => {
-        tg.BackButton.show();
-        tg.BackButton.onClick(() => {
-            navigate(-1)
-        })
-        return () => {
-            tg.BackButton.hide()
+        if (tg.BackButton) {
+            tg.BackButton.show();
+
+            const handleBackClick = () => {
+                navigate(-1);
+            };
+
+            tg.BackButton.onClick(handleBackClick);
+
+            return () => {
+                tg.BackButton.offClick(handleBackClick);
+                tg.BackButton.hide();
+            };
         }
-    }, [])
+    }, [navigate]);
 
     return (
         <div className='wrapper'>
             {contextHolder}
             <h1 className={s.nick}>ПРОМОКОД</h1>
             <div className={s.input}>
-                <Input placeholder='Введите промокод'/>
+                <Input placeholder='Введите промокод' />
                 <Button onClick={success}>
                     {isLoading ? 'Подождите...' : 'Активировать'}
                 </Button>
             </div>
         </div>
-    )
-}
+    );
+};
